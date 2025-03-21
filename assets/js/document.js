e38516782ef67e69582b6032d7b576bf3da9d5a3
Sujit Barui <sujitbarui2017@gmail.com>
2025-03-21 18:46:08 +0530

    files added

---
+++ assets/js/document.js
@@ -0,0 +1,137 @@
+gsap.to(".gsap-stacking-pages", {
+  y: "-100%",
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: ".gsap-stacked-trigger",
+    pin: true,
+    scrub: true,
+    start: "0%",
+    end: "top -400%",
+    markers : true,
+  }
+});
+
+function createAnimation(element) {
+  const tl = gsap.timeline({ paused: true, delay: 1 });
+
+  tl.to(element, {
+    scaleY: 0,
+    duration: 0.15,
+    ease: "cubic-bezier(0.5, 1.51, 0.55, 0.89)"
+  })
+    .to(element, {
+      scaleY: 1,
+      duration: 0.15,
+      ease: "cubic-bezier(0.5, 1.51, 0.55, 0.89)"
+    })
+    .to(element, {
+      scaleY: 0,
+      duration: 0.15,
+      ease: "cubic-bezier(0.5, 1.51, 0.55, 0.89)"
+    })
+    .to(element, {
+      scaleY: 1,
+      duration: 0.15,
+      ease: "cubic-bezier(0.5, 1.51, 0.55, 0.89)"
+    });
+
+  ScrollTrigger.create({
+    trigger: element,
+    start: "top 50%",
+    end: "top 20%",
+    onEnter: () => tl.play(),
+  });
+
+}
+
+createAnimation(".section-6-svg");
+createAnimation(".section-10-svg");
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+window.onload = () => {
+  document.querySelector(".marquee-magic").style.display = "none";
+  setTimeout(() => {
+    document.querySelector(".marquee-magic").style.display = "flex";
+  }, 50);
+};
+
+
+gsap.to(".section-9", {
+  scrollTrigger: {
+    trigger: ".section-9",
+    start: "top top",
+    end: "top -100%",
+    pin: true,
+    pinSpacing: false,
+  }
+});
+
+gsap.from(".section-9img", {
+  filter: "blur(10px)",
+  duration : 2,
+  scrollTrigger: {
+    trigger : ".section-9img img",
+    start : "top 50%",
+    end : "top -100%"
+  }
+})
+
+function scrollRotate() {
+  let image = document.getElementById("abc");
+  image.style.transform = "rotate(" + window.pageYOffset/8 + "deg)";
+}
+
+window.onscroll = function () {
+  scrollRotate();
+};
+
+
+(function() {
+
+  'use strict';
+
+  const breakpoint = window.matchMedia( '(min-width:768px)' );
+
+  let mySwiper;
+
+  const breakpointChecker = function() {
+    if ( breakpoint.matches === true ) {
+
+	  if ( mySwiper !== undefined ) mySwiper.destroy( true, true );
+
+	  return;
+      } else if ( breakpoint.matches === false ) {
+
+        return enableSwiper();
+
+      }
+  };
+
+  const enableSwiper = function() {
+
+    mySwiper = new Swiper(".mySwiper-special", {
+
+      slidesPerView: "auto",
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+      },
+    });
+
+  };
+
+  breakpoint.addListener(breakpointChecker);
+
+  breakpointChecker();
+
+})();
+
